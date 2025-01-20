7c9eafa16d4e88f9c2b1e034605ed937ae1961d8
Luis Lorenzs <luislorenz7@gmail.com>
2025-01-20 18:12:19 +0100

    1

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sudoku_solver
+
+# principle
+- input a sudoku: 
+    - format: nested list
+- algorithm that checks:
+    - in row
+    - in square 
+- print solution 
--- sudoku_solver_v1.py
+++ sudoku_solver_v1.py
@@ -14,6 +14,8 @@ example_board = [
         [1, -1, 9,   -1, -1, -1,   2, -1, -1]
     ]
 
+# -1 represents an empty cell 
+
 # checking 
 # row 
     # there are multiple checks 
@@ -21,8 +23,101 @@ example_board = [
 # iteration: through all horizontal lists, verticial list, suares
     # then repeat
 
-def is_valid(): 
-    pass 
+rows = 9
+cols = 9 
+
+def find_empty():
+    for row in range(rows):
+        for col in range(cols):
+            if example_board[row][col] == -1:
+                empty_spot = (row, col)
+                return empty_spot
+    
+
+square_1 = [(0,0), (0,1), (0,2),  (1,0), (1,1), (1,2),  (2,0), (2,1), (2,2)]
+
+square_2 = [(0,4), (0,5), (0,6),  (1,4), (1,5), (1,6),  (2,4), (2,5), (2,6)]
+
+square_3 = [(0,7), (0,8), (0,9),  (1,7), (1,8), (1,9),  (2,7), (2,8), (2,9)]
+
+square_4 = [(4,0), (4,1), (4,2),  (5,0), (5,1), (5,2),  (6,0), (6,1), (6,2)]
+
+square_5 = [(4,4), (4,5), (4,6),  (5,4), (5,5), (5,6),  (6,4), (6,5), (6,6)]
+
+square_6 = [(4,7), (4,8), (4,9),  (5,7), (5,8), (5,9),  (6,7), (6,8), (6,9)]
+
+square_7 = [(7,0), (7,1), (7,2),  (8,0), (8,1), (8,2),  (9,0), (9,1), (9,2)]
+
+square_8 = [(7,4), (7,5), (7,6),  (8,4), (8,5), (8,6),  (9,4), (9,5), (9,6)] 
+
+square_9 = [(7,7), (7,8), (7,9),  (8,7), (8,8), (8,9),  (9,7), (9,8), (9,9)]
+
+
+def is_valid(empty_spot, num): 
+    row, col = empty_spot
+
+    # check row
+    if num in example_board[row]: 
+        return False
+
+    # colum
+    col_list = []
+    col_list.clear()
+    for row in range(rows):
+        col_list.append(example_board[row][col])
+
+    # check col
+    if num in col_list: 
+        return False
+    
+    # square
+    square = None
+    if (empty_spot) in square_1:
+        square = square_1
+    elif (empty_spot) in square_2:
+        square = square_2
+    elif (empty_spot) in square_3:
+        square = square_3
+    elif (empty_spot) in square_4:
+        square = square_4
+    elif (empty_spot) in square_5:
+        square = square_5
+    elif (empty_spot) in square_6:
+        square = square_6
+    elif (empty_spot) in square_7:
+        square = square_7
+    elif (empty_spot) in square_8:
+        square = square_8
+    elif (empty_spot) in square_9:
+        square = square_9
+    
+    # square_list
+    square_list = []
+    square_list.clear()
+    for elements in square:
+        r, c = elements
+        square_list.append(example_board[r][c])
+
+    # check 3x3
+    if num in square_list:
+        return False
+    return True 
+
+def recursive_solving():
+    pass
+    # get empty spot
+    # set num in empty spot
+    # chech validation 
+        # False
+            # new number/spot
+        # True
+            # new number for next spot
+    # final solution
+        # False
+            # there are still empty spots
+            # go back ...???
+        # True
+            # all valid and no empty spots
 
 def game(): 
     # intro
